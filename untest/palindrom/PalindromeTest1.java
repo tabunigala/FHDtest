a9a86b150f2afc4ce0dc8344cca0e1e38189268e
anfre005 <ananyafrederickdkl@outlook.com>
2019-11-19 12:29:47 +0100

    Testing commands in Suite

---
+++ untest/palindrom/PalindromeTest1.java
@@ -0,0 +1,18 @@
+package palindrom;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ palindromTest.class,palindromecase2.class })
+
+public class PalindromeTest1 {
+	
+
+}
